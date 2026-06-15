86ee9bf285db0279d9f8c4aa8c4962342b1dcd9d
pallavithegod <jainpallavi.delhi@gmail.com>
2026-06-15 21:25:36 +0530

    use: postman

--- practice/src/app.js
+++ practice/src/app.js
@@ -2,4 +2,25 @@ const express = require('express')
 
 const app = express()
 
+const entity = [];
+
+app.use(express.json())
+
+app.post('/database', (req, res) =>{
+    entity.push(req.body)             // takes input from postman
+    
+    res.status(201).json({
+        message: "instance created successfully"
+    })
+
+    console.log(req.body)
+})
+
+app.get("/database", (req, res) => {
+    res.status(200).json({
+        entity: entity,
+        message: "records printed successfully"
+    })
+})
+
 module.exports = app;
