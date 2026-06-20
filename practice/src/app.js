820998b5df13e259369a702be73045ff9d52d668
pallavithegod <jainpallavi.delhi@gmail.com>
2026-06-20 18:50:19 +0530

    advanced requests

--- practice/src/app.js
+++ practice/src/app.js
@@ -7,7 +7,7 @@ const entity = [];
 app.use(express.json())
 
 app.post('/database', (req, res) =>{
-    entity.push(req.body)             // takes input from postman
+    entity.push(req.body)             // takes input from postman ie frontend
     
     res.status(201).json({
         message: "instance created successfully"
@@ -16,10 +16,32 @@ app.post('/database', (req, res) =>{
     console.log(req.body)
 })
 
-app.get("/database", (req, res) => {
+app.get("/database", (req, res) => {     //prints on frontend
     res.status(200).json({
         entity: entity,
         message: "records printed successfully"
+    })
+})
+
+// doesn't delete the index, only makes the object = null
+app.delete("/database/:index", (req, res) => {   //index is var (dynamic)
+
+    const index = req.params.index
+    delete entity[index]
+
+    res.status(200).json({
+        message: "record deleted successfully"
+    })
+})
+
+app.patch("/database/:index", (req, res) => {
+    const index = req.params.index
+    const description = req.body.age
+
+    entity[index].age = description
+   
+    res.status(201).json({
+        message: "instance changed in server successfully"
     })
 })
 
